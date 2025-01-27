3d6fd6e0d6ae4a8411d48bb7166c8bd1cf55de1c
Edwin Chiu <edwinchiu@EdwindeMacBook-Air-4.local>
2025-01-27 15:31:49 +0800

    implemented the firestore database.

--- script.js
+++ script.js
@@ -79,9 +79,10 @@ const addBookBtn = document.getElementById('addBookBtn');
 const bookModal = document.querySelector('.bookModal');
 const overlay = document.getElementById('overlay');
 const errorMsg = document.getElementById('errorMsg');
-const loggedIn = document.getElementById('loggedIn')
-const loggedOut = document.getElementById('loggedOut')
-const loadingRing = document.getElementById('loadingRing')
+const accountModal = document.getElementById('accountModal');
+const loggedIn = document.getElementById('loggedIn');
+const loggedOut = document.getElementById('loggedOut');
+const loadingRing = document.getElementById('loadingRing');
 
 
 const openAddBookModal = () => {
@@ -154,18 +155,37 @@ function changeStatus(book) {
     }
 }
 
+const setupNavbar = (user) => {
+    if (user) {
+      loggedIn.classList.add('active')
+      loggedOut.classList.remove('active')
+    } else {
+      loggedIn.classList.remove('active')
+      loggedOut.classList.add('active')
+    }
+    loadingRing.classList.remove('active')
+}
+
+const setupAccountModal = (user) => {
+    if (user) {
+      accountModal.innerHTML = `
+        <p>Logged in as</p>
+        <p><strong>${user.email.split('@')[0]}</strong></p>`;
+    } else {
+      accountModal.innerHTML = '';
+    }
+}
+
 render();
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-analytics.js";
-import { getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut} from "firebase/auth";
+import { getFirestore, collection, query, where, orderBy, onSnapshot, addDoc, doc, updateDoc, deleteDoc, serverTimestamp} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const auth = getAuth();
 
@@ -176,10 +196,84 @@ onAuthStateChanged(auth, (user) => {
     if (user) {
         setupRealTimeListener();
     } else {
-        if (unsubscribe) unsubscribe()
-        restoreLocal()
-        updateBooksGrid()
+        if (unsubscribe) unsubscribe();
+        render();
     }
-    setupAccountModal(user)
-    setupNavbar(user)
+    setupAccountModal(user);
+    setupNavbar(user);
 })
+
+const signIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+}
+  
+const signOut = () => {
+    signOut(auth);
+}
+  
+logInBtn.onclick = signIn;
+logOutBtn.onclick = signOut;
+
+const db = getFirestore(app);
+let unsubscribe;
+
+const setupRealTimeListener = () => {
+    const booksQuery = query(
+        collection(db, "books"),
+        where("ownerId", "==", auth.currentUser.uid),
+        orderBy("createdAt")
+    );
+
+    unsubscribe = onSnapshot(booksQuery, (snapshot) => {
+        library.books = docsToBooks(snapshot.docs);
+        updateBooksGrid();
+    }, (error) => {
+        console.error("Error fetching books: ", error);
+    });
+}
+
+const addBookDB = async (newBook) => {
+    await addDoc(collection(db, "books"), bookToDoc(newBook));
+}
+
+const removeBookDB = async (title) => {
+    const bookId = await getBookIdDB(title);
+    await deleteDoc(doc(db, "books", bookId));
+}
+
+const toggleBookIsReadDB = async (book) => {
+    const bookId = await getBookIdDB(book.name);
+    await updateDoc(doc(db, 'books', bookId), { isRead: !book.isRead });
+}
+
+const getBookIdDB = async (name) => {
+    const booksQuery = query(
+        collection(db, 'books'),
+        where('ownerId', '==', auth.currentUser.uid),
+        where('name', '==', name)
+    );
+    const snapshot = await getDocs(booksQuery);
+    const bookId = snapshot.docs[0].id; 
+    return bookId;
+}
+
+const docsToBooks = (docs) => {
+    return docs.map((doc) => {
+      return new Book(
+        doc.data().name,
+        doc.data().author,
+        doc.data().isRead
+      )
+    })
+}
+
+const bookToDoc = (book) => {
+    return {
+      ownerId: auth.currentUser.uid,
+      name: book.name,
+      author: book.author,
+      isRead: book.isRead,
+      createdAt: serverTimestamp(),
+    }
+}
